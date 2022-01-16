3b52f591ee514cb135b5d0247ab1a06490b95b5a
kx_01 <90087463+kx0101@users.noreply.github.com>
2022-01-16 20:56:20 +0200

    Update [id].js

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -5,9 +5,7 @@ export default async function handler(req, res) {
   const {
     method,
     query: { id },
-    cookies
   } = req;
-  const token = cookies.token
 
   await dbConnect();
 
@@ -21,9 +19,6 @@ export default async function handler(req, res) {
   }
 
   if (method === "PUT") {
-    if(!token || token !== process.env.token){
-      return res.status(401).json("Not authenticated!")
-    }
     try {
       const product = await Product.findByIdAndUpdate(id, req.body, {
         new: true,
@@ -35,9 +30,6 @@ export default async function handler(req, res) {
   }
 
   if (method === "DELETE") {
-    if(!token || token !== process.env.token){
-      return res.status(401).json("Not authenticated!")
-    }
     try {
       await Product.findByIdAndDelete(id);
       res.status(200).json("The product has been deleted!");
