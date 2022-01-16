97a0d67643dd28efbb97b282422f3920c4715d21
kx_01 <90087463+kx0101@users.noreply.github.com>
2022-01-16 21:16:06 +0200

    Update index.js

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -4,8 +4,6 @@ import Product from "../../../models/Product";
 export default async function handler(req, res) {
   const { method, cookies } = req;
 
-  const token = cookies.token
-
   await dbConnect();
 
   if (method === "GET") {
@@ -18,9 +16,6 @@ export default async function handler(req, res) {
   }
 
   if (method === "POST") {
-    if(!token || token !== process.env.token){
-      return res.status(401).json("Not authenticated!")
-    }
     try {
       const product = await Product.create(req.body);
       res.status(201).json(product);
