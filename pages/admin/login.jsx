1a8abc8594127940a04026f4cf49e28e2f1ee842
kx_01 <90087463+kx0101@users.noreply.github.com>
2022-01-16 12:09:25 +0200

    Update login.jsx

--- pages/admin/login.jsx
+++ pages/admin/login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
 
     const handleClick = async () => {
         try {
-            await axios.post("http://localhost:3000/api/login", {
+            await axios.post("https://pizzawebsite-main.vercel.app/api/login", {
                 username, 
                 password,
             });
